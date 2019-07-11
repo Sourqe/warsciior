7e213f99f41150bfa4d2392123c193e12d83d2aa
Nick <nickvandewaterlaat@hotmail.com>
2019-07-11 13:36:02 +0200

    Added new spells with targeting

--- src/engine.py
+++ src/engine.py
@@ -1,15 +1,16 @@
-import tcod as libtcod
+import libtcodpy as libtcod
 
 from components.fighter import Fighter
+from components.inventory import Inventory
 from death_functions import kill_monster, kill_player
 from entity import Entity, get_blocking_entities_at_location
-from input_handlers import handle_keys
-from map_objects.game_map import GameMap
-from render_functions import clear_all, render_all, RenderOrder
-from game_states import GameStates
-from components.inventory import Inventory
 from fov_functions import initialize_fov, recompute_fov
 from game_messages import Message, MessageLog
+from game_states import GameStates
+from input_handlers import handle_keys, handle_mouse
+from map_objects.game_map import GameMap
+from render_functions import clear_all, render_all, RenderOrder
+
 
 def main():
     screen_width = 80
@@ -23,9 +24,11 @@ def main():
     message_width = screen_width - bar_width - 2
     message_height = panel_height - 1
 
+    # Size of the map
     map_width = 80
     map_height = 43
 
+    # Some variables for the rooms in the map
     room_max_size = 10
     room_min_size = 6
     max_rooms = 30
@@ -47,20 +50,22 @@ def main():
     fighter_component = Fighter(hp=30, defense=2, power=5)
     inventory_component = Inventory(26)
     player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
-                fighter=fighter_component, inventory=inventory_component)
+                    fighter=fighter_component, inventory=inventory_component)
     entities = [player]
 
     libtcod.console_set_custom_font('../assets/images/arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
 
-    libtcod.console_init_root(screen_width, screen_height, 'Warrsciior', False)
+    libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     con = libtcod.console_new(screen_width, screen_height)
     panel = libtcod.console_new(screen_width, panel_height)
 
     game_map = GameMap(map_width, map_height)
-    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities, max_monsters_per_room, max_items_per_room)
+    game_map.make_map(max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
+                      max_monsters_per_room, max_items_per_room)
 
     fov_recompute = True
+
     fov_map = initialize_fov(game_map)
 
     message_log = MessageLog(message_x, message_width, message_height)
@@ -71,30 +76,36 @@ def main():
     game_state = GameStates.PLAYERS_TURN
     previous_game_state = game_state
 
+    targeting_item = None
+
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS | libtcod.EVENT_MOUSE, key, mouse)
 
         if fov_recompute:
             recompute_fov(fov_map, player.x, player.y, fov_radius, fov_light_walls, fov_algorithm)
-        
+
         render_all(con, panel, entities, player, game_map, fov_map, fov_recompute, message_log, screen_width,
-            screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
+                   screen_height, bar_width, panel_height, panel_y, mouse, colors, game_state)
 
         fov_recompute = False
 
         libtcod.console_flush()
 
         clear_all(con, entities)
-        
+
         action = handle_keys(key, game_state)
+        mouse_action = handle_mouse(mouse)
 
         move = action.get('move')
         pickup = action.get('pickup')
         show_inventory = action.get('show_inventory')
-        inventory_index = action.get('inventory_index')
         drop_inventory = action.get('drop_inventory')
+        inventory_index = action.get('inventory_index')
         exit = action.get('exit')
         fullscreen = action.get('fullscreen')
+
+        left_click = mouse_action.get('left_click')
+        right_click = mouse_action.get('right_click')
 
         player_turn_results = []
 
@@ -130,22 +141,34 @@ def main():
             previous_game_state = game_state
             game_state = GameStates.SHOW_INVENTORY
 
-        if inventory_index is not None and previous_game_state != GameStates.PLAYER_DEAD and inventory_index < len(
-                player.inventory.items):
-            item = player.inventory.items[inventory_index]
-            
-            if game_state == GameStates.SHOW_INVENTORY:
-                player_turn_results.extend(player.inventory.use(item))
-            elif game_state == GameStates.DROP_INVENTORY:
-                player_turn_results.extend(player.inventory.drop_item(item))
-        
         if drop_inventory:
             previous_game_state = game_state
             game_state = GameStates.DROP_INVENTORY
 
+        if inventory_index is not None and previous_game_state != GameStates.PLAYER_DEAD and inventory_index < len(
+                player.inventory.items):
+            item = player.inventory.items[inventory_index]
+
+            if game_state == GameStates.SHOW_INVENTORY:
+                player_turn_results.extend(player.inventory.use(item, entities=entities, fov_map=fov_map))
+            elif game_state == GameStates.DROP_INVENTORY:
+                player_turn_results.extend(player.inventory.drop_item(item))
+
+        if game_state == GameStates.TARGETING:
+            if left_click:
+                target_x, target_y = left_click
+
+                item_use_results = player.inventory.use(targeting_item, entities=entities, fov_map=fov_map,
+                                                        target_x=target_x, target_y=target_y)
+                player_turn_results.extend(item_use_results)
+            elif right_click:
+                player_turn_results.append({'targeting_cancelled': True})
+
         if exit:
             if game_state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY):
                 game_state = previous_game_state
+            elif game_state == GameStates.TARGETING:
+                player_turn_results.append({'targeting_cancelled': True})
             else:
                 return True
 
@@ -158,6 +181,8 @@ def main():
             item_added = player_turn_result.get('item_added')
             item_consumed = player_turn_result.get('consumed')
             item_dropped = player_turn_result.get('item_dropped')
+            targeting = player_turn_result.get('targeting')
+            targeting_cancelled = player_turn_result.get('targeting_cancelled')
 
             if message:
                 message_log.add_message(message)
@@ -182,6 +207,19 @@ def main():
                 entities.append(item_dropped)
 
                 game_state = GameStates.ENEMY_TURN
+
+            if targeting:
+                previous_game_state = GameStates.PLAYERS_TURN
+                game_state = GameStates.TARGETING
+
+                targeting_item = targeting
+
+                message_log.add_message(targeting_item.item.targeting_message)
+
+            if targeting_cancelled:
+                game_state = previous_game_state
+
+                message_log.add_message(Message('Targeting cancelled'))
 
         if game_state == GameStates.ENEMY_TURN:
             for entity in entities:
@@ -208,9 +246,9 @@ def main():
 
                     if game_state == GameStates.PLAYER_DEAD:
                         break
-
             else:
                 game_state = GameStates.PLAYERS_TURN
+
 
 if __name__ == '__main__':
     main()
--- src/map_objects/game_map.py
+++ src/map_objects/game_map.py
@@ -1,16 +1,21 @@
-import tcod as libtcod
+import libtcodpy as libtcod
+from random import randint
 
 from components.ai import BasicMonster
 from components.fighter import Fighter
 from components.item import Item
 
-from map_objects.tile import Tile
-from map_objects.rectangle import Rect
-
 from entity import Entity
-from random import randint
-from item_functions import heal
+
+from game_messages import Message
+
+from item_functions import cast_confuse, cast_fireball, cast_lightning, heal
+
+from map_objects.rectangle import Rect
+from map_objects.tile import Tile
+
 from render_functions import RenderOrder
+
 
 class GameMap:
     def __init__(self, width, height):
@@ -24,12 +29,11 @@ class GameMap:
         return tiles
 
     def make_map(self, max_rooms, room_min_size, room_max_size, map_width, map_height, player, entities,
-        max_monsters_per_room, max_items_per_room):
-
+                 max_monsters_per_room, max_items_per_room):
         rooms = []
         num_rooms = 0
 
-        for _ in range(max_rooms):
+        for r in range(max_rooms):
             # random width and height
             w = randint(room_min_size, room_max_size)
             h = randint(room_min_size, room_max_size)
@@ -97,45 +101,66 @@ class GameMap:
             self.tiles[x][y].blocked = False
             self.tiles[x][y].block_sight = False
 
-    def is_blocked(self, x, y):
-        if self.tiles[x][y].blocked:
-            return True
-
-        return False
-
     def place_entities(self, room, entities, max_monsters_per_room, max_items_per_room):
         # Get a random number of monsters
         number_of_monsters = randint(0, max_monsters_per_room)
+
+        # Get a random number of items
         number_of_items = randint(0, max_items_per_room)
 
-        for _ in range(number_of_monsters):
+        for i in range(number_of_monsters):
             # Choose a random location in the room
             x = randint(room.x1 + 1, room.x2 - 1)
             y = randint(room.y1 + 1, room.y2 - 1)
 
+            # Check if an entity is already in that location
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
                 if randint(0, 100) < 80:
                     fighter_component = Fighter(hp=10, defense=0, power=3)
                     ai_component = BasicMonster()
 
                     monster = Entity(x, y, 'o', libtcod.desaturated_green, 'Orc', blocks=True,
-                        render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
+                                     render_order=RenderOrder.ACTOR, fighter=fighter_component, ai=ai_component)
                 else:
                     fighter_component = Fighter(hp=16, defense=1, power=4)
                     ai_component = BasicMonster()
 
                     monster = Entity(x, y, 'T', libtcod.darker_green, 'Troll', blocks=True, fighter=fighter_component,
-                        render_order=RenderOrder.ACTOR, ai=ai_component)
+                                     render_order=RenderOrder.ACTOR, ai=ai_component)
 
                 entities.append(monster)
-        
-        for _ in range(number_of_items):
+
+        for i in range(number_of_items):
             x = randint(room.x1 + 1, room.x2 - 1)
             y = randint(room.y1 + 1, room.y2 - 1)
 
             if not any([entity for entity in entities if entity.x == x and entity.y == y]):
-                item_component = Item(use_function=heal, amount=4)
-                item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM,
-                    item=item_component)
+                item_chance = randint(0, 100)
+
+                if item_chance < 70:
+                    item_component = Item(use_function=heal, amount=4)
+                    item = Entity(x, y, '!', libtcod.violet, 'Healing Potion', render_order=RenderOrder.ITEM,
+                                  item=item_component)
+                elif item_chance < 80:
+                    item_component = Item(use_function=cast_fireball, targeting=True, targeting_message=Message(
+                        'Left-click a target tile for the fireball, or right-click to cancel.', libtcod.light_cyan),
+                        damage=12, radius=3)
+                    item = Entity(x, y, '#', libtcod.red, 'Fireball Scroll', render_order=RenderOrder.ITEM,
+                            item=item_component)
+                elif item_chance < 90:
+                    item_component = Item(use_function=cast_confuse, targeting=True, targeting_message=Message(
+                        'Left-click an enemy to confuse it, or right-click to cancel.', libtcod.light_cyan))
+                    item = Entity(x, y, '#', libtcod.light_pink, 'Confusion Scroll', render_order=RenderOrder.ITEM,
+                            item=item_component)
+                else:
+                    item_component = Item(use_function=cast_lightning, damage=20, maximum_range=5)
+                    item = Entity(x, y, '#', libtcod.yellow, 'Lightning Scroll', render_order=RenderOrder.ITEM,
+                        item=item_component)
 
                 entities.append(item)
+
+    def is_blocked(self, x, y):
+        if self.tiles[x][y].blocked:
+            return True
+
+        return False
